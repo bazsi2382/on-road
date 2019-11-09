c7fb907843e148ea99bddf3983322ddcc6fb12d1
Miklós Balázs <bazsi2382@gmail.com>
2019-11-09 14:52:29 +0100

    first commit

---
+++ src/main/java/com/codecool/onroad/OnRoadApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.onroad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnRoadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnRoadApplication.class, args);
+    }
+
+}
